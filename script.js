92821a4b5593f427c2a6a80a99f6733d4ad1143e
Luis Esquivel <luisesquivel97@hotmail.com>
2023-01-22 19:30:54 -0600

    terminado

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-$(document).ready()
+$(document).ready();
 {
+  $("#ButtonPost").click(TodoPost);
+  $("#ButtonClear").click(TodoClear);
+  $("#ButtonMark").click(TodoMark);
+  $("#ButtonDelete").click(TodoDelete);
 
-  $("ButtonPost").click(TodoPost)
-  $("ButtonClear").click(TodoClear)
-  $("ButtonMark").click(TodoMark)
-  $("ButtonDelete").click(TodoDelete)
-  
- function TodoPost(){
-   let todo=$("CajaTexto").val()
-   alert(todo)   
- }
- function TodoClear(){}
- function TodoMark(){}
- function TodoDelete(){}
- 
+  function TodoPost(e) {
+    e.preventDefault();
+    let todo = $("#CajaTexto").val();
+    $("#todoList").append(
+      `<input type="checkbox" name="caja"/> <lable>${todo}</lable> <br>   `
+    );
+  }
+  function TodoMark() {
+    let todos = $("[name='caja']");
+    for (let i = 0; i < todos.length; i++) {
+      todos[i].checked = true;
+    }
+  }
+  function TodoClear() {
+    let todos = $("[name='caja']");
+    for (let i = 0; i < todos.length; i++) {
+      todos[i].checked = false;
+    }
+  }
+  function TodoDelete() {
+    $("#todoList").html("");
+  }
 }
